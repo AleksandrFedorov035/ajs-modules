962081c1e2f025815496dde695780785813ee508
Александр Носов <149945914+AleksandrFedorov035@users.noreply.github.com>
2025-01-19 16:37:53 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js'
+    },
+    entry: './src/index.js',
+    mode: 'development',
+    module: {
+        rules:  [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.txt$/,
+                loader: "raw-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'main.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+
+}
